3e45e599b107c43a4e2c2f9cce51b999cf600021
Hiep Cao <hcao7@csc.com>
2016-11-30 18:59:07 +0700

    re init

---
+++ src/component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'gota-demo',
+    template: '<h4>Hello World Component</h4>'
+})
+export class GotaDemo {
+    constructor() {
+
+    }
+}
